315ecde79cd5ae3b466d886789fa9e01e45e5d48
OzAvichayTaizi <OzAvichayTaizi@gmail.com>
2020-01-31 18:00:27 +0200

    update bid

--- src/main/java/aplication/services/ManagerBid.java
+++ src/main/java/aplication/services/ManagerBid.java
@@ -45,11 +45,18 @@ public class ManagerBid {
     public Boolean updateBid(@NotNull String bidId, @NotNull Bid.Status status)
     {
         Bid bid = bidRepository.findById(bidId).orElse(null);
-        if (bid == null || Bid.Status.UNKNOWN == status) return false;
+        if (bid == null ||
+            Bid.Status.UNKNOWN == status ||
+            bid.getStatus() != Bid.Status.UNKNOWN)
+            return false;
+
+        bid.setStatus(status);
         if(status == Bid.Status.LOSE){
             long budget = hashMapCampaign.get(bid.getCampaignId()) + bid.getCost();
             hashMapCampaign.put(bid.getCampaignId(),budget);
         }
+
+        bidRepository.save(bid);
 
         return  true;
     }
